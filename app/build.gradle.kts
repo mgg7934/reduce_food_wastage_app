52494b7aab0ef0d3dd547dca92db27f823b1dc65
mgg7934 <57641528+mgg7934@users.noreply.github.com>
2024-10-20 15:52:25 +1300

    background added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.reduce_food_wastage_app"
-        minSdk = 34
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
